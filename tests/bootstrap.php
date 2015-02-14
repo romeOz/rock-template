4c7a2a4652bb85a003facab4e7ed60412926f7f7
romeOz <serggalka@gmail.com>
2015-02-14 09:41:41 +0300

    Remove xdebug config.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,8 +8,7 @@ if (is_file($composerAutoload)) {
 $loader->addPsr4('rockunit\\', __DIR__);
 
 defined('ROCKUNIT_RUNTIME') or define('ROCKUNIT_RUNTIME', __DIR__ . '/runtime');
-ini_set('xdebug.var_display_max_data', '50000');
-ini_set('xdebug.var_display_max_depth', '10');
+
 $_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'site.com';
 $_SERVER['REQUEST_URI'] = '/';
 $_SERVER['REMOTE_ADDR'] = '127.0.0.1';
